5b803520100cc060d395500c14929526412fedcc
preetdhabi <preetdhabi@gmail.com>
2014-08-05 18:20:13 -0700

    A test Commit !

---
+++ src/org/example/recicalc/Recipe.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.example.recicalc;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author Preet
+ */
+public class Recipe {
+    private String recipeName;
+    private BigDecimal price;
+    private String recipeType;
+}
